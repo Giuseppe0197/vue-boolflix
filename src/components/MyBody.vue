1076af820b292f69d4c5558a167fce50f218ff8a
Giuseppe0197 <faro.g@hotmail.it>
2021-12-01 12:18:23 +0100

    inizializzazione e prime modifiche

---
+++ src/components/MyBody.vue
@@ -0,0 +1,29 @@
+<template>
+  <main>
+
+  </main>
+</template>
+
+<script>
+
+
+export default {
+  name: 'MyBody',
+  data() {
+      return {
+          serchVar: "",
+          apiUrl: "https://api.themoviedb.org/3/search/movie?api_key=8531cd4b5660045d3ad85863d06b1d4b&language=en-US&query=" + this.serchVar + "&page=1&include_adult=false"
+      }
+  },
+
+}
+</script>
+
+<style lang="scss">
+
+main {
+    height: calc(100vh - 80px);
+    background-color: red;
+}
+
+</style>
---
+++ src/components/MyHeader.vue
@@ -0,0 +1,55 @@
+<template>
+  <header>
+      <h1>Boolflix</h1>
+
+      <div id="form">
+        <input type="text" placeholder="Cerca un film">
+        <button>Cerca</button>
+      </div>
+
+      
+  </header>
+</template>
+
+<script>
+/* import HelloWorld from './components/HelloWorld.vue' */
+
+export default {
+  name: 'MyHeader',
+  components: {
+    
+  }
+}
+</script>
+
+<style lang="scss">
+    header {
+        height: 80px;
+        background-color: black;
+
+        h1 {
+            color: red;
+            padding-top: 20px;
+            float: left;
+        }
+
+        #form {
+            float: right;
+            margin-right: 20px;
+            margin-top: 30px;
+
+            input {
+                margin-right: 20px;
+            }
+            button {
+                padding: 5px;
+                background-color: transparent;
+                border: 2px solid white;
+                border-radius: 10px;
+                color: white;
+                cursor: pointer;
+            }
+        }
+
+    }
+</style>
